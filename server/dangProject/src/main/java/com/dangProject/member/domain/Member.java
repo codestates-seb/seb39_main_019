6b07ce7539aa846cf7f96544eb2c3bb6d504bf62
Damm06 <ghkeka111@gmail.com>
2022-09-16 19:45:52 +0900

    feat: 지역구 샘플 데이터 추가

--- server/dangProject/src/main/java/com/dangProject/member/domain/Member.java
+++ server/dangProject/src/main/java/com/dangProject/member/domain/Member.java
@@ -1,4 +1,44 @@
 package com.dangProject.member.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+    private String password;
+    private String nickname;
+
+    @Lob
+    private String defaultIcon;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+
+    private RegionName guName;
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
 }
