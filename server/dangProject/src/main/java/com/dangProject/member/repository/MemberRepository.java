4ab0d12254fa12170a96294cd01751fed7d44c7f
Dam06 <ghkeka111@gmail.com>
2022-09-20 00:30:53 +0900

    test: re-clone push 테스트

--- server/dangProject/src/main/java/com/dangProject/member/repository/MemberRepository.java
+++ server/dangProject/src/main/java/com/dangProject/member/repository/MemberRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+//    Optional<Member> findMemberByEmail(String email);
 }
