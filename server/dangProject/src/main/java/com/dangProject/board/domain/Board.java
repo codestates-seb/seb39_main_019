424ec7dc3a3d7ff0cf6893d7a74351e37b93aeee
“Sohyeon” <yeahh2483@gmail.com>
2022-09-20 11:36:52 +0900

    feat: Board id

--- server/dangProject/src/main/java/com/dangProject/board/domain/Board.java
+++ server/dangProject/src/main/java/com/dangProject/board/domain/Board.java
@@ -1,4 +1,14 @@
 package com.dangProject.board.domain;
 
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class Board {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
 }
