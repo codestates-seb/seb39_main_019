bb86e1703fbb83b38794d20ab22b71945b1fae8e
Dam06 <ghkeka111@gmail.com>
2022-09-22 17:13:01 +0900

    feat: member 수정

--- server/dangProject/src/main/java/com/dangProject/DangProjectApplication.java
+++ server/dangProject/src/main/java/com/dangProject/DangProjectApplication.java
@@ -2,10 +2,7 @@ package com.dangProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableJpaAuditing
 @SpringBootApplication
