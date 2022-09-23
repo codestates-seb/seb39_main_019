2d788fa96a9009f03f29dd8517b19db08e96e784
Dam06 <ghkeka111@gmail.com>
2022-09-23 10:18:38 +0900

    feat: dog 추가

--- server/dangProject/src/main/java/com/dangProject/dog/dto/DogResponse.java
+++ server/dangProject/src/main/java/com/dangProject/dog/dto/DogResponse.java
@@ -1,10 +1,25 @@
 package com.dangProject.dog.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class DogResponse {
+
+//    private int reqNo;
+//    private int resultCode;
+//    private String resultMsg;
+//    private String errorMsg;
+    private Long dogRegNo;
+//    private Long rfidCd;
+    private String dogNm;
+//    private String sexNm;
+//    private String kindNm; //품종
+//    private String neuterYn; //중성화 여부
+//    private String orgNm; //담당기관명
+//    private String officeTel; //기관 전화번호
+//    private String aprGbNm; //승인여부
 
 }
---
+++ server/dangProject/src/main/java/com/dangProject/dog/repository/DogRepository.java
@@ -0,0 +1,7 @@
+package com.dangProject.dog.repository;
+
+import com.dangProject.dog.domain.Dog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DogRepository extends JpaRepository<Dog, Long> {
+}
