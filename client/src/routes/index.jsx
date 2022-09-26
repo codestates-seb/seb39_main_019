5f7fff52ad9a8838f6418fbfa0015c1e7247aa6a
HelloHailie <103437860+HelloHailie@users.noreply.github.com>
2022-09-26 11:03:59 +0900

    Revert "Dev"

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,56 +1,20 @@
-import Login from "../page/Login";
-import Signup from "../page/Signup";
-import Main from "../page/Main";
-import Post from "../page/Post";
-import Randing from "../page/Randing";
-import PuppyAuthentication from "../page/PuppyAuthentication";
-import PuppyInfo from "../page/PuppyInfo";
-
-import PpAuthDoneMdl from "../components/Modal/PpAuthDoneMdl";
-import PpAuthFailMdl from "../components/Modal/PpAuthFailMdl";
-import PpAuthOvlMdl from "../components/Modal/PpAuthOvlMdl";
+import Login from '../page/Login'
+import SignUp from '../page/Signup'
+import Main from '../page/Main'
 
 const RouteModule = [
   {
-    path: "/signup",
-    element: <Signup />,
+    path:"/signup",
+    element:<SignUp/>
   },
   {
-    path: "/login",
-    element: <Login />,
-  },
+    path:"/login",
+    element:<Login/>
+  }, 
   {
-    path: "/main",
-    element: <Main />,
-  },
-  {
-    path: "/post",
-    element: <Post />,
-  },
-  {
-    path: "/",
-    element: <Randing />,
-  },
-  {
-    path: "/puppyauthentication",
-    element: <PuppyAuthentication />,
-  },
-  {
-    path: "/puppyinfo",
-    element: <PuppyInfo />,
-  },
-  {
-    path: "/ppauthdonemdl",
-    element: <PpAuthDoneMdl />,
-  },
-  {
-    path: "/ppauthfailmdl",
-    element: <PpAuthFailMdl />,
-  },
-  {
-    path: "/ppauthovlmdl",
-    element: <PpAuthOvlMdl />,
-  },
-];
+    path:"/",
+    element:<Main/>
+  }
+]
 
 export default RouteModule;
--- client/src/store/filter.jsx
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-const useStore = create((set)=>({
-  index:0,
-  setIndex:((input)=>set({index:input}))
-}))
-
-export default useStore
