2fa8dc2ee9a57ec22426d01b829d02d6085f76b0
열정영웅 <103996469+houndhollis@users.noreply.github.com>
2022-09-26 14:03:26 +0900

    Update globalStore.jsx

---
+++ client/src/store/globalStore.jsx
@@ -0,0 +1,14 @@
+import create from "zustand";
+import axios from "axios";
+
+const useStore = create((set) => ({
+
+
+
+  isLight : true,
+  setIsLight : () => set((state)=>({isLight: !state.isLight}))
+  
+}))
+
+
+export default useStore
