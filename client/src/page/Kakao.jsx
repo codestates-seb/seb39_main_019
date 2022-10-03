661ae70259281b065fbe2d85259c860cd7170023
HelloHailie <alsltjsals@gmail.com>
2022-10-03 18:20:05 +0900

    test:카카오소셜로그인테스트2

--- client/src/page/Kakao.jsx
+++ client/src/page/Kakao.jsx
@@ -3,10 +3,14 @@ import axios from "axios";
 
 const Kakao = () => {
   let code = new URL(window.location.href).searchParams.get("code");
-
-  axios
-    .get(`http://localhost:5173/oauth2/login/callback/kakao?code=${code}`)
-    .then((res) => console.log(res))
+  console.log(code);
+  axios({
+    method: "GET",
+    url: `http://43.200.20.180:8080/oauth2/login/callback/kakao?code=${code}`,
+  })
+    .then((res) => {
+      console.log(res);
+    })
     .catch((err) => console.log(err));
 
   return <div>잠시만 기다려 주세요! 로그인 중입니다.</div>;
--- client/src/secretData.js
+++ client/src/secretData.js
@@ -1,4 +1,5 @@
-export const REST_API_KEY = "9b73e6a94d50abd34a4a5c36b684dcb6";
+// export const REST_API_KEY = "5d281d095e3337b0537f8c6f92258d90";
+export const REST_API_KEY = "eb0c19048f33bc796afa6c397904fbad";
 export const REDIRECT_URI = "http://localhost:5173/oauth2/login/callback/kakao";
 export const GOOGLE_CLIENT_ID =
   "1066203673720-r1jd9pdto9ilufmov7gfn4fpc93jnfan.apps.googleusercontent.com";
